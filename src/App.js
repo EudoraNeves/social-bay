d0750963192b5a7f29f28d68316d71a9e4f0fdb1
Haina Zheng <eudoraneves@outlook.com>
2022-10-22 16:25:06 +0200

    add static content to sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import Header from './components/header/Header';
+import Sidebar from './components/body/Sidebar';
+import Feed from './components/body/Feed';
+import Widget from './components/body/Widget';
 
 function App() {
   return (
@@ -7,9 +10,14 @@ function App() {
       {/* Header */}
       <Header />
       {/* App Body */}
-      {/* Side Bar */}
-      {/* Feed */}
-      {/* Widget */}
+      <div className="app__body">
+        {/* Side Bar */}
+        <Sidebar username="Hai Na" userTitle="Junior Web Developer" visitorsNum="300" postViewersNum="3" />
+        {/* Feed */}
+        <Feed />
+        {/* Widget */}
+        <Widget />
+      </div>
     </div>
   );
 }
